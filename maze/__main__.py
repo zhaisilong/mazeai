74bdb13fe4d5aa173e3e96d9e638b63431ceb6a0
zhaisilong <zhaisilong@outlook.com>
2024-04-19 16:09:51 +0800

    first commit

---
+++ maze/__main__.py
@@ -0,0 +1,6 @@
+def main():
+    print("this is the main function for maze.ai")
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,52 @@
+import io
+import os
+import re
+
+from pathlib import Path
+from setuptools import find_packages
+from setuptools import setup
+
+
+def read(filename):
+    filename = os.path.join(os.path.dirname(__file__), filename)
+    text_type = type(u"")
+    with io.open(filename, mode="r", encoding='utf-8') as fd:
+        return re.sub(text_type(r':[a-z]+:`~?(.*?)`'), text_type(r'``\1``'), fd.read())
+
+
+def version():
+    """ Get the local package version. """
+    namespace = {}
+    path = Path("maze/__version__.py")
+    exec(path.read_text(), namespace)
+    return namespace["__version__"]
+
+
+setup(
+    name="mazeai",
+    version=version(),
+    url="https://github.com/zhaisilong/mazeai",
+    license='MIT',
+    author="Zhai Silong",
+    author_email="zhaisilong@outlook.com",
+    description="A General Tool Set for Science Researches",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    packages=find_packages(where='.', exclude=('tests',), include=('*')),
+    package_data={'maze': ['resource/*']},
+    entry_points={
+        'console_scripts': [
+            'maze = maze.__main__:main',
+        ],
+    },
+    install_requires=[
+        
+    ],
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+    ],
+    python_requires='>=3.6',  # The recommended least version of python
+)
