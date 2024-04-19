cd3166fb4aa61ae91bf0b25da22e1b3c2959c51a
zhaisilong <zhaisilong@outlook.com>
2024-04-20 00:38:37 +0800

    ..

--- docs/conf.py
+++ docs/conf.py
@@ -72,39 +72,39 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
-on_rtd = os.environ.get("READTHEDOCS", None) == "True"
+# on_rtd = os.environ.get("READTHEDOCS", None) == "True"
 
-if on_rtd:
-    latex_elements = {
-        # The paper size ('letterpaper' or 'a4paper').
-        # 'papersize': 'letterpaper',
-        # The font size ('10pt', '11pt' or '12pt').
-        # 'pointsize': '10pt',
-        # Additional stuff for the LaTeX preamble.
-        "preamble": r"""
-    \hypersetup{unicode=true}
-    \usepackage{CJKutf8}
-    \DeclareUnicodeCharacter{00A0}{\nobreakspace}
-    \DeclareUnicodeCharacter{2203}{\ensuremath{\exists}}
-    \DeclareUnicodeCharacter{2200}{\ensuremath{\forall}}
-    \DeclareUnicodeCharacter{2286}{\ensuremath{\subseteq}}
-    \DeclareUnicodeCharacter{2713}{x}
-    \DeclareUnicodeCharacter{27FA}{\ensuremath{\Longleftrightarrow}}
-    \DeclareUnicodeCharacter{221A}{\ensuremath{\sqrt{}}}
-    \DeclareUnicodeCharacter{221B}{\ensuremath{\sqrt[3]{}}}
-    \DeclareUnicodeCharacter{2295}{\ensuremath{\oplus}}
-    \DeclareUnicodeCharacter{2297}{\ensuremath{\otimes}}
-    \begin{CJK}{UTF8}{gbsn}
-    \AtEndDocument{\end{CJK}}
-    """,
-    }
-else:
-    latex_elements = {
-        "papersize": "a4paper",
-        "utf8extra": "",
-        "inputenc": "",
-        "babel": r"""\usepackage[english]{babel}""",
-        "preamble": r"""
-        \usepackage{ctex}
-        """,
-    }
+# if on_rtd:
+#     latex_elements = {
+#         # The paper size ('letterpaper' or 'a4paper').
+#         # 'papersize': 'letterpaper',
+#         # The font size ('10pt', '11pt' or '12pt').
+#         # 'pointsize': '10pt',
+#         # Additional stuff for the LaTeX preamble.
+#         "preamble": r"""
+#     \hypersetup{unicode=true}
+#     \usepackage{CJKutf8}
+#     \DeclareUnicodeCharacter{00A0}{\nobreakspace}
+#     \DeclareUnicodeCharacter{2203}{\ensuremath{\exists}}
+#     \DeclareUnicodeCharacter{2200}{\ensuremath{\forall}}
+#     \DeclareUnicodeCharacter{2286}{\ensuremath{\subseteq}}
+#     \DeclareUnicodeCharacter{2713}{x}
+#     \DeclareUnicodeCharacter{27FA}{\ensuremath{\Longleftrightarrow}}
+#     \DeclareUnicodeCharacter{221A}{\ensuremath{\sqrt{}}}
+#     \DeclareUnicodeCharacter{221B}{\ensuremath{\sqrt[3]{}}}
+#     \DeclareUnicodeCharacter{2295}{\ensuremath{\oplus}}
+#     \DeclareUnicodeCharacter{2297}{\ensuremath{\otimes}}
+#     \begin{CJK}{UTF8}{gbsn}
+#     \AtEndDocument{\end{CJK}}
+#     """,
+#     }
+# else:
+#     latex_elements = {
+#         "papersize": "a4paper",
+#         "utf8extra": "",
+#         "inputenc": "",
+#         "babel": r"""\usepackage[english]{babel}""",
+#         "preamble": r"""
+#         \usepackage{ctex}
+#         """,
+#     }
